78a62c859e814b17acc9e004aff4ad13efba0e8b
yazan dahood <dahood.yazan8@gmail.com>
2025-04-12 11:42:02 +0300

    Update Knight.h

--- Chess/include/Knight.h
+++ Chess/include/Knight.h
@@ -1,19 +1,19 @@
 #ifndef CHESS_KNIGHT_H
 #define CHESS_KNIGHT_H
-#include <string>
 
 #include "Piece.h"
+#include <string>
 
 class Knight : public Piece {
 private:
     char type;
 
 public:
-    Knight(char t) {
+    Knight(char t, bool isWhite) : Piece(isWhite) {
         type = t;
     }
 
-    bool is_legel_movement(const string &input, const string &board, bool isWhiteTurn) override;
+    bool is_legel_movement(const std::string &input, const std::string &board, bool isWhiteTurn) override;
 
     char get_type() override;
 };
