fe2ae6ef37d3f1d259ef98214d413522bb1e103b
yazan dahood <dahood.yazan8@gmail.com>
2025-04-12 11:50:58 +0300

    Update Piece.cpp

--- Chess/src/Piece.cpp
+++ Chess/src/Piece.cpp
@@ -4,15 +4,6 @@ Piece::Piece(bool isWhite) {
     this->isWhite = isWhite;
 }
 
-bool Piece::is_white() const {
+bool Piece::isWhitePiece() const {
     return isWhite;
-}
-
-// Default movement - should be overridden
-bool Piece::is_legel_movement(const std::string &input, const std::string &board, bool isWhiteTurn) {
-    return false;
-}
-
-char Piece::get_type() {
-    return type;
 }
