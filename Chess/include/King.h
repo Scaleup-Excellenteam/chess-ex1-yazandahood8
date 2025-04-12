3e5473046017d49e818d98ca482a268bf75217e0
yazan dahood <dahood.yazan8@gmail.com>
2025-04-12 11:11:15 +0300

    Update King.h

--- Chess/include/King.h
+++ Chess/include/King.h
@@ -2,12 +2,20 @@
 #define KING_H
 
 #include "Piece.h"
+#include <string>
 
 class King : public Piece {
+private:
+    char type;
+
 public:
-    King(bool isWhite) : Piece(isWhite) {}
-    char getSymbol() const override { return isWhite ? 'K' : 'k'; }
-    bool isValidMove(int sx, int sy, int dx, int dy, const Board& board) const override;
+    King(char t, bool isWhite) : Piece(isWhite) {
+        type = t;
+    }
+
+    bool is_legel_movement(const std::string &input, const std::string &board, bool isWhiteTurn) override;
+
+    char get_type() override;
 };
 
-#endif
+#endif // KING_H
