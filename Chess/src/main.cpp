52addfee6c3f737cebc679314c5b5f32f5463c30
yazan dahood <dahood.yazan8@gmail.com>
2025-04-12 11:17:55 +0300

    Revert "Add full solution for exercise 2 - polymorphism chess"

--- Chess/src/main.cpp
+++ Chess/src/main.cpp
@@ -1,27 +1,40 @@
-// main.cpp
+// Chess 
 #include "Chess.h"
-#include "Game.h"
-#include <sstream>
 
 int main()
 {
-    std::string board = "RNBQKBNRPPPPPPPP################################pppppppprnbqkbnr";
-    Chess a(board);
-    Game game(board);
+	string board = "RNBQKBNRPPPPPPPP################################pppppppprnbqkbnr"; 
+//	string board = "##########K###############################R#############r#r#####";
+	Chess a(board);
+	int codeResponse = 0;
+	string res = a.getInput();
+	while (res != "exit")
+	{
+		/* 
+		codeResponse value : 
+		Illegal movements : 
+		11 - there is not piece at the source  
+		12 - the piece in the source is piece of your opponent
+		13 - there one of your pieces at the destination 
+		21 - illegal movement of that piece 
+		31 - this movement will cause you checkmate
 
-    int codeResponse = 0;
-    std::string res = a.getInput();
-    while (res != "exit")
-    {
-        std::string from, to;
-        std::stringstream ss(res);
-        ss >> from >> to;
+		legal movements : 
+		41 - the last movement was legal and cause check 
+		42 - the last movement was legal, next turn 
+		*/
 
-        codeResponse = game.tryMove(from, to);
-        a.setCodeResponse(codeResponse);
-        res = a.getInput();
-    }
+		/**/ 
+		{ // put your code here instead that code
+			cout << "code response >> ";
+			cin >> codeResponse;
+		}
+		/**/
 
-    std::cout << std::endl << "Exiting " << std::endl;
-    return 0;
+		a.setCodeResponse(codeResponse);
+		res = a.getInput(); 
+	}
+
+	cout << endl << "Exiting " << endl; 
+	return 0;
 }
