ad2812a9bbe40dbb52cfa9a1a44ce48792f28105
yazan dahood <dahood.yazan8@gmail.com>
2025-04-29 22:58:25 +0300

    Update main.cpp

--- Chess/src/main.cpp
+++ Chess/src/main.cpp
@@ -1,7 +1,9 @@
 // main.cpp
 #include "Chess.h"
 #include "Game.h"
+#include "MoveRecommender.h"
 #include <sstream>
+#include <iostream>
 
 int main()
 {
@@ -11,6 +13,7 @@ int main()
 
     int codeResponse = 0;
     std::string res = a.getInput();
+
     while (res != "exit")
     {
         std::string from, to;
@@ -18,10 +21,18 @@ int main()
         ss >> from >> to;
 
         codeResponse = game.tryMove(from, to);
+
         a.setCodeResponse(codeResponse);
+
+        // כאן המלצת מהלכים לפני קבלת הקלט הבא
+        if (codeResponse == 42 || codeResponse == 41) { // מהלך חוקי
+            std::cout << "\nRecommended moves:\n";
+            MoveRecommender::recommendMoves(game, 2); // עומק 2
+        }
+
         res = a.getInput();
     }
 
-    std::cout << std::endl << "Exiting " << std::endl;
+    std::cout << "\nExiting...\n";
     return 0;
 }
