35adeecb6b0df1a6b2fafcf7c0380cf3463c6502
yazan dahood <dahood.yazan8@gmail.com>
2025-04-29 23:07:26 +0300

    Update Knight.h

--- Chess/include/Knight.h
+++ Chess/include/Knight.h
@@ -5,9 +5,13 @@
 
 class Knight : public Piece {
 public:
-    Knight(bool isWhite);
-    char getSymbol() const override;
-    bool isValidMove(int sx, int sy, int dx, int dy, const Board& board) const override;
+    Knight(bool isWhite) : Piece(isWhite) {}
+    char getSymbol() const override { return isWhite ? 'N' : 'n'; }
+
+    bool isValidMove(int sx, int sy, int dx, int dy, const Board& board) const override {
+        int dxAbs = std::abs(sx - dx), dyAbs = std::abs(sy - dy);
+        return (dxAbs == 2 && dyAbs == 1) || (dxAbs == 1 && dyAbs == 2);
+    }
 };
 
 #endif
