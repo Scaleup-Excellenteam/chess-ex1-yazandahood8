f0136e6c14184a3984e631503267fa70559832f0
yazan dahood <dahood.yazan8@gmail.com>
2025-05-06 19:22:37 +0300

    Update Queen.h

--- Chess/include/Queen.h
+++ Chess/include/Queen.h
@@ -6,7 +6,7 @@ class Queen : public Piece {
 public:
     Queen(bool isWhite): Piece(isWhite) {}
     ~Queen() {}
-    bool areSquaresLegal(int srcRow, int srcCol, int destRow, int destCol, Piece* boardMove[8][8]) override;
+    bool isLegalMove(int srcRow, int srcCol, int destRow, int destCol, Piece* boardMove[8][8]) override;
 private:
     char getPiece() const override { return isWhite ? 'Q' : 'q'; }
 
