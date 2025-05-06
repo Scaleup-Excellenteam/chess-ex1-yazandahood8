48b9818f5e958c2c3124583a6a83a0b7a41b3bbf
yazan dahood <dahood.yazan8@gmail.com>
2025-05-06 19:43:12 +0300

    Update Pawn.h

--- Chess/include/Pawn.h
+++ Chess/include/Pawn.h
@@ -6,4 +6,6 @@ public:
     Pawn(bool isWhite, int row, int col) : Piece(isWhite, row, col) {}
     char getPiece() const override { return isWhite ? 'P' : 'p'; }
     bool isLegalMove(int destRow, int destCol, Piece* board[8][8]) override;
+bool areSquaresLegal(int srcRow, int srcCol, int destRow, int destCol, Piece* board[8][8]);
+
 };
