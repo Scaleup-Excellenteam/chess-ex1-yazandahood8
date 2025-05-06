0d862e142bee7d8fe1eb7f4e4807af867b4e8780
yazan dahood <dahood.yazan8@gmail.com>
2025-05-06 19:31:16 +0300

    Update Piece.h

--- Chess/include/Piece.h
+++ Chess/include/Piece.h
@@ -2,29 +2,20 @@
 
 class Piece {
 public:
-    Piece(bool isWhite, int row = -1, int col = -1)
-        : isWhite(isWhite), row(row), col(col) {}
-
+    Piece(bool isWhite, int row = -1, int col = -1) : isWhite(isWhite), row(row), col(col) {}
     virtual ~Piece() {}
 
     bool getColor() const { return isWhite; }
-
     int getRow() const { return row; }
     int getCol() const { return col; }
-
-    void setPosition(int r, int c) {
-        row = r;
-        col = c;
-    }
-
-    virtual bool isLegalMove(int destRow, int destCol, Piece* board[8][8]) = 0;
-
-    virtual bool isLegalMove(int destRow, int destCol) { return false; }
+    void setPosition(int r, int c) { row = r; col = c; }
 
     virtual char getPiece() const = 0;
 
+    virtual bool isLegalMove(int destRow, int destCol, Piece* board[8][8]) = 0;
+
 protected:
     bool isWhite;
-    int row;
-    int col;
+    int row, col;
 };
+
