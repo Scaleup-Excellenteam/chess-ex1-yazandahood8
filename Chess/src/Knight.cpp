24b19925bd6b8bf379e1f3ef9aa7ec8db1ea6e31
yazan dahood <dahood.yazan8@gmail.com>
2025-05-06 20:17:37 +0300

    Update Knight.cpp

--- Chess/src/Knight.cpp
+++ Chess/src/Knight.cpp
@@ -1,7 +1,7 @@
 #include "Knight.h"
 #include <cmath>
 
-bool Knight::isLegalMove(int destRow, int destCol, Piece* board[8][8]) {
+bool Knight::isLegalMove(int destRow, int destCol, Piece* /*board*/[8][8]) {
     int dRow = std::abs(destRow - row);
     int dCol = std::abs(destCol - col);
     return (dRow == 2 && dCol == 1) || (dRow == 1 && dCol == 2);
