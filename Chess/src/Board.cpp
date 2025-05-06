d873e3adbd090fbe2503d637455978a9ea4a7d0b
yazan dahood <dahood.yazan8@gmail.com>
2025-05-06 19:36:40 +0300

    Update Board.cpp

--- Chess/src/Board.cpp
+++ Chess/src/Board.cpp
@@ -1,11 +1,11 @@
 #include "Board.h"
-#include <cctype>    
-#include <memory>   
+#include <cctype>
+#include <memory>
 
 Board::Board() {
     for (int i = 0; i < 8; ++i)
         for (int j = 0; j < 8; ++j)
-            boardMove[i][j] = std::make_unique<Rook>(true, 0, 0);
+            boardMove[i][j] = nullptr;
 }
 
 Board::~Board() {}
@@ -14,34 +14,34 @@ void Board::loadFromString(const std::string& boardString) {
     if (boardString.size() != 64)
         return;
 
-    for (int i = 0; i < 8; ++i)
+    for (int i = 0; i < 8; ++i) {
         for (int j = 0; j < 8; ++j) {
             int index = i * 8 + j;
             char symbol = boardString[index];
 
-            boardMove[i][j] = nullptr;  
+            boardMove[i][j] = nullptr;
 
             char lower = std::tolower(symbol);
-            bool turn = isupper(symbol); 
+            bool isWhite = std::isupper(symbol);
 
             switch (lower) {
                 case 'r':
-                    boardMove[i][j] = std::make_unique<Rook>(turn);
+                    boardMove[i][j] = std::make_unique<Rook>(isWhite, i, j);
                     break;
                 case 'k':
-                    boardMove[i][j] = std::make_unique<King>(turn);
+                    boardMove[i][j] = std::make_unique<King>(isWhite, i, j);
                     break;
                 case 'b':
-                    boardMove[i][j] = std::make_unique<Bishop>(turn);
+                    boardMove[i][j] = std::make_unique<Bishop>(isWhite, i, j);
                     break;
                 case 'q':
-                    boardMove[i][j] = std::make_unique<Queen>(turn);
+                    boardMove[i][j] = std::make_unique<Queen>(isWhite, i, j);
                     break;
                 case 'n':
-                    boardMove[i][j] = std::make_unique<Knight>(turn);
+                    boardMove[i][j] = std::make_unique<Knight>(isWhite, i, j);
                     break;
                 case 'p':
-                    boardMove[i][j] = std::make_unique<Pawn>(turn);
+                    boardMove[i][j] = std::make_unique<Pawn>(isWhite, i, j);
                     break;
                 case '#':
                 default:
@@ -49,8 +49,8 @@ void Board::loadFromString(const std::string& boardString) {
                     break;
             }
         }
+    }
 }
-
 
 std::string Board::getBoardString() const {
     std::string result;
