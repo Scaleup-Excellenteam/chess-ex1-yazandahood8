47f37a81048e95297fb1855a67b0c9381cd5bd73
yazan dahood <dahood.yazan8@gmail.com>
2025-05-06 19:58:04 +0300

    Update Queen.cpp

--- Chess/src/Queen.cpp
+++ Chess/src/Queen.cpp
@@ -1,7 +1,6 @@
 #include "Queen.h"
 #include "Rook.h"
 #include "Bishop.h"
-#include "ValidatMove.h"
 
 bool Queen::areSquaresLegal(int srcRow, int srcCol, int destRow, int destCol, Piece* boardMove[8][8]) {
     Rook rookHelper(isWhite, row, col);
@@ -10,6 +9,6 @@ bool Queen::areSquaresLegal(int srcRow, int srcCol, int destRow, int destCol, Pi
            bishopHelper.isLegalMove(destRow, destCol, boardMove);
 }
 
-    return rookHelper.isLegalMove(srcRow, srcCol, destRow, destCol, boardMove) ||
-           bishopHelper.isLegalMove(srcRow, srcCol, destRow, destCol, boardMove);
+bool Queen::isLegalMove(int destRow, int destCol, Piece* boardMove[8][8]) {
+    return areSquaresLegal(row, col, destRow, destCol, boardMove);
 }
