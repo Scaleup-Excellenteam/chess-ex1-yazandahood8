41872028e773e1354e261c6c232e9713410f3472
yazan dahood <dahood.yazan8@gmail.com>
2025-05-06 20:09:46 +0300

    Update Pawn.cpp

--- Chess/src/Pawn.cpp
+++ Chess/src/Pawn.cpp
@@ -1,28 +1,28 @@
 #include "Pawn.h"
 #include <cmath>
-#include <iostream>
-#include <ostream>
 
 bool Pawn::areSquaresLegal(int srcRow, int srcCol, int destRow, int destCol, Piece* boardMove[8][8]) {
+    if (destRow < 0 || destRow >= 8 || destCol < 0 || destCol >= 8)
+        return false;
+
     int direction = isWhite ? 1 : -1;
     int startRow = isWhite ? 1 : 6;
 
-
     if (srcCol == destCol) {
+        // One step forward
         if (destRow == srcRow + direction && boardMove[destRow][destCol] == nullptr)
             return true;
 
-        // First move - 2 steps forward
-        if (srcRow == startRow && destRow == srcRow + 2 * direction &&
+        // First move: two steps forward
+        if (srcRow == startRow &&
+            destRow == srcRow + 2 * direction &&
             boardMove[srcRow + direction][srcCol] == nullptr &&
             boardMove[destRow][destCol] == nullptr)
             return true;
-
-
     }
 
-    // Capture diagonally
-    if (std::abs(destCol - srcCol) == 1 && destRow == srcRow + direction &&
+    if (std::abs(destCol - srcCol) == 1 &&
+        destRow == srcRow + direction &&
         boardMove[destRow][destCol] != nullptr &&
         boardMove[destRow][destCol]->getColor() != isWhite)
         return true;
