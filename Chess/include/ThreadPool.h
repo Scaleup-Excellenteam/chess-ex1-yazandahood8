3ba006fb509e52968f3c4f637a87556432b932fd
yazan dahood <dahood.yazan8@gmail.com>
2025-05-24 22:32:50 +0300

    upload ThreadPool.h

---
+++ Chess/include/ThreadPool.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include <thread>
+#include <queue>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+
+class ThreadPool {
+public:
+    explicit ThreadPool(size_t numThreads);
+    ~ThreadPool();
+
+    void enqueue(std::function<void()> task);
+    void shutdown();
+
+private:
+    void workerLoop();
+
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+    std::mutex      queueMutex;
+    std::condition_variable cv;
+    std::atomic<bool> stopFlag;
+};
