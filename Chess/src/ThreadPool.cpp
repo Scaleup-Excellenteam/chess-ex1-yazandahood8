7bcdd13e9f33f38a35de811ca879f83c96016ffe
yazan dahood <dahood.yazan8@gmail.com>
2025-05-25 23:18:03 +0300

    Update ThreadPool.cpp

--- Chess/src/ThreadPool.cpp
+++ Chess/src/ThreadPool.cpp
@@ -1,41 +1,60 @@
 #include "ThreadPool.h"
 
 ThreadPool::ThreadPool(size_t numThreads)
-    : stopFlag(false)
 {
+    stopFlag = false;
+
     for (size_t i = 0; i < numThreads; ++i) {
-        workers.emplace_back(&ThreadPool::workerLoop, this);
+        std::thread worker;
+        worker = std::thread(&ThreadPool::workerLoop, this);
+        workers.push_back(std::move(worker));
     }
 }
 
-ThreadPool::~ThreadPool() {
+ThreadPool::~ThreadPool()
+{
     shutdown();
 }
 
-void ThreadPool::enqueue(std::function<void()> task) {
-    {
-        std::lock_guard<std::mutex> lock(queueMutex);
-        tasks.push(std::move(task));
-    }
+void ThreadPool::enqueue(std::function<void()> task)
+{
+    std::lock_guard<std::mutex> lock(queueMutex);
+    tasks.push(std::move(task));
     cv.notify_one();
 }
 
-void ThreadPool::workerLoop() {
+void ThreadPool::workerLoop()
+{
     while (true) {
         std::function<void()> task;
 
         {
             std::unique_lock<std::mutex> lock(queueMutex);
+
             cv.wait(lock, [this] {
-                std::lock_guard<std::mutex> stopLock(stopMutex);
-                return stopFlag || !tasks.empty();
+                bool shouldWake = false;
+
+                {
+                    std::lock_guard<std::mutex> stopLock(stopMutex);
+                    if (stopFlag || !tasks.empty()) {
+                        shouldWake = true;
+                    }
+                }
+
+                return shouldWake;
             });
+
+            bool shouldExit = false;
 
             {
                 std::lock_guard<std::mutex> stopLock(stopMutex);
                 if (stopFlag && tasks.empty()) {
-                    return;
+                    shouldExit = true;
                 }
+            }
+
+            if (shouldExit) {
+                return;
             }
 
             task = std::move(tasks.front());
@@ -46,7 +65,8 @@ void ThreadPool::workerLoop() {
     }
 }
 
-void ThreadPool::shutdown() {
+void ThreadPool::shutdown()
+{
     {
         std::lock_guard<std::mutex> lock1(queueMutex);
         std::lock_guard<std::mutex> lock2(stopMutex);
@@ -55,7 +75,9 @@ void ThreadPool::shutdown() {
 
     cv.notify_all();
 
-    for (auto& t : workers) {
+    for (size_t i = 0; i < workers.size(); ++i) {
+        std::thread& t = workers[i];
+
         if (t.joinable()) {
             t.join();
         }
