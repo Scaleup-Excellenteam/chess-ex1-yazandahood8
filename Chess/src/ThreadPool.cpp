4e0396312ea083ef818aef13d349ba32692a740b
yazan dahood <dahood.yazan8@gmail.com>
2025-05-25 23:14:50 +0300

    Update ThreadPool.cpp

--- Chess/src/ThreadPool.cpp
+++ Chess/src/ThreadPool.cpp
@@ -3,8 +3,9 @@
 ThreadPool::ThreadPool(size_t numThreads)
     : stopFlag(false)
 {
-    for (size_t i = 0; i < numThreads; ++i)
+    for (size_t i = 0; i < numThreads; ++i) {
         workers.emplace_back(&ThreadPool::workerLoop, this);
+    }
 }
 
 ThreadPool::~ThreadPool() {
@@ -22,24 +23,43 @@ void ThreadPool::enqueue(std::function<void()> task) {
 void ThreadPool::workerLoop() {
     while (true) {
         std::function<void()> task;
+
         {
             std::unique_lock<std::mutex> lock(queueMutex);
-            cv.wait(lock, [this]{ return stopFlag || !tasks.empty(); });
-            if (stopFlag && tasks.empty()) return;
+            cv.wait(lock, [this] {
+                std::lock_guard<std::mutex> stopLock(stopMutex);
+                return stopFlag || !tasks.empty();
+            });
+
+            {
+                std::lock_guard<std::mutex> stopLock(stopMutex);
+                if (stopFlag && tasks.empty()) {
+                    return;
+                }
+            }
+
             task = std::move(tasks.front());
             tasks.pop();
         }
+
         task();
     }
 }
 
 void ThreadPool::shutdown() {
     {
-        std::lock_guard<std::mutex> lock(queueMutex);
+        std::lock_guard<std::mutex> lock1(queueMutex);
+        std::lock_guard<std::mutex> lock2(stopMutex);
         stopFlag = true;
     }
+
     cv.notify_all();
-    for (auto &t : workers)
-        if (t.joinable()) t.join();
+
+    for (auto& t : workers) {
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+
     workers.clear();
 }
