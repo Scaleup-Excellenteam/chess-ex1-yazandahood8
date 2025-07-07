3819a3101bd842f3939f90fac3cb20cd2c6735a4
yazan dahood <dahood.yazan8@gmail.com>
2025-07-07 16:36:39 +0300

    Delete Chess/include/King.h

--- Chess/include/King.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Piece.h"
-
-class King : public Piece {
-public:
-    King(bool isWhite, int row, int col) : Piece(isWhite, row, col) {}
-    char getPiece() const override { return isWhite ? 'K' : 'k'; }
-    bool isLegalMove(int destRow, int destCol, Piece* board[8][8]) override;
-};
