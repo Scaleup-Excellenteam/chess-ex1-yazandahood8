9fa454f2e780ba2c8daef0cb0cc378a27c6f8fd7
yazan dahood <dahood.yazan8@gmail.com>
2025-07-07 16:36:48 +0300

    Delete Chess/include/Knight.h

--- Chess/include/Knight.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Piece.h"
-
-class Knight : public Piece {
-public:
-    Knight(bool isWhite, int row, int col) : Piece(isWhite, row, col) {}
-    char getPiece() const override { return isWhite ? 'N' : 'n'; }
-    bool isLegalMove(int destRow, int destCol, Piece* board[8][8]) override;
-};
