e63befbdd76d424d13c0916076807ae2f7f53d55
yazan dahood <dahood.yazan8@gmail.com>
2025-07-07 16:35:38 +0300

    Delete Chess/include/Bishop.h

--- Chess/include/Bishop.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Piece.h"
-
-class Bishop : public Piece {
-public:
-    Bishop(bool isWhite, int row, int col) : Piece(isWhite, row, col) {}
-    char getPiece() const override { return isWhite ? 'B' : 'b'; }
-    bool isLegalMove(int destRow, int destCol, Piece* board[8][8]) override;
-};
